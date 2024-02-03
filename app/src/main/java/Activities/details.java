fa2de92637578f399025954e1b5be1a2486724fa
edwin <ricky.aranas@bisu.edu.ph>
2024-02-04 04:00:07 +0800

    Feb 04,3:58 pagbabalik

---
+++ app/src/main/java/Activities/details.java
@@ -0,0 +1,64 @@
+package Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.example.corn.R;
+
+import Domains.PopularDomain;
+
+public class details extends AppCompatActivity {
+    private TextView desctxt, titletxt, bedtxt,guidetxt,wifitxt, titletext, diseasename;
+    private PopularDomain item;
+    private ImageView backbtn,  picImg;
+        @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+
+        initView();
+        setVariable();
+    }
+
+    private void setVariable() {
+        item = (PopularDomain) getIntent().getSerializableExtra("object");
+
+        titletext.setText(item.getTitle());
+        desctxt.setText(item.getDescription());
+
+        if(item.isGuide()) {
+            guidetxt.setText("Guide");
+        }else{
+            guidetxt.setText("no guide");
+        }
+        if(item.isWifi()) {
+            wifitxt.setText("Wifi");
+        }else{
+            wifitxt.setText("no wifi");
+        }
+        int drawableResId = getResources().getIdentifier(item.getPic(), "drawable", getPackageName());
+
+        Glide.with(this)
+                .load(drawableResId)
+                .into(picImg);
+        backbtn.setOnClickListener(v -> finish());
+
+
+    }
+
+    private void initView() {
+            titletext = findViewById(R.id.titletext);
+       // titletxt=findViewById(R.id.titleTxt);
+        desctxt=findViewById(R.id.desctxt);
+        bedtxt = findViewById(R.id.bedtxt);
+        guidetxt=findViewById(R.id.guidetxt);
+        wifitxt=findViewById(R.id.wifitxt);
+        picImg=findViewById(R.id.picImg);
+        backbtn=findViewById(R.id.backbtn);
+    }
+}
